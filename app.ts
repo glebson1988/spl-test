e0bef95dd49bcf1946950782bf6a1db36140bdf8
glebson1988 <glebson1988@yandex.ru>
2025-01-27 20:42:01 +0100

    Starting some preparing stuff

--- app.ts
+++ app.ts
@@ -1,1 +1,19 @@
+import { Connection, Keypair, PublicKey, sendAndConfirmTransaction, SystemProgram, Transaction } from "@solana/web3.js";
+import {
+    getOrCreateAssociatedTokenAccount,
+    getMinimumBalanceForRentExemptMint,
+    getAssociatedTokenAddressSync,
+    MINT_SIZE, TOKEN_PROGRAM_ID,
+    createInitializeMintInstruction,
+    createMintToInstruction,
+    createAssociatedTokenAccountIdempotentInstruction,
+    transfer,
+    burnChecked
+} from "@solana/spl-token";
+import * as dotenv from "dotenv";
 
+dotenv.config();
+
+const secret = JSON.parse(process.env.SECRET_KEY || "[]");
+const tokenAuthority = Keypair.fromSecretKey(new Uint8Array(secret));
+const receiver = Keypair.generate();
